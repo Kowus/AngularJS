a8911e4f7c6c5604b1f39308a1d71864f3ab1138
Jai <jaiprak@thoughtworks.com>
2015-04-17 18:47:54 +0530

    Example for filter in AngularJS is data is modified.

--- JS/app_control1.js
+++ JS/app_control1.js
@@ -1,6 +1,9 @@
 var app = angular.module('myapp',[]);		
 app.controller('app_control', function($scope){
-		$scope.employees = [{name:'James Bond',city:'New York'},
+		$scope.employees = [{name:'George Washington',city:'Washington'},
 		{name:'Leonidas',city:'Sparta'},
+		{name:'Asoka',city:'Ujjain'},
+		{name:'Genghis Khan',city:'Mangolia'},
+		{name:'Hitler',city:'Berlin'},
 		{name:'Jai Prakash',city:'Lucknow'}];
 });
