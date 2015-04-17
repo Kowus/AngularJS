c797863fff18474dc435404106fadc55ea7643c4
Jai <jaiprak@thoughtworks.com>
2015-04-17 18:30:14 +0530

    Example for filter in AngularJS is added.

--- JS/app_control1.js
+++ JS/app_control1.js
@@ -1,6 +1,6 @@
 var app = angular.module('myapp',[]);		
 app.controller('app_control', function($scope){
-		$scope.employees = [{name:'Jai Prakash',city:'Lucknow'},
-		{name:'James Bond',city:'New York'},
-		{name:'Leonidas',city:'Sparta'}];
+		$scope.employees = [{name:'James Bond',city:'New York'},
+		{name:'Leonidas',city:'Sparta'},
+		{name:'Jai Prakash',city:'Lucknow'}];
 });
