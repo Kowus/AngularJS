23533a8559371adb5d425286b6da812453d1a671
Jai <jaiprak@thoughtworks.com>
2015-05-03 18:53:16 +0530

    Filters demo 2 is  created.

--- JS/demoapp.js
+++ JS/demoapp.js
@@ -1,5 +1,19 @@
 (function(){
 	var app = angular.module("basic",[]);
+
+	var elements = [{name:"Hydrogen",AN:1},
+					{name:"Helium",AN:2},
+					{name:"Lithium",AN:3},
+					{name:"Beryllium",AN:4},
+					{name:"Boron",AN:5},
+					{name:"Carbon",AN:6},
+					{name:"Nitrogen",AN:7},
+					{name:"Oxygen",AN:8}];
+
+	app.controller('ShowElements', function($scope){
+		$scope.elements = elements;
+	});
+
 	app.controller('JustAlert', function(){
 		alert("Are you crazy... This is devil's code..");
 	});
